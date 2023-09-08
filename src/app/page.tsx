588db49f497717c4659757d2683d97d31063e0ee
Pedro Gomes <pedro-gomes@ibm.com>
2023-09-08 14:54:41 -0300

    feat: create basic main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
 import { Button } from "@/components/ui/button";
+import { SignIn, auth } from "@clerk/nextjs";
+import { LogIn } from "lucide-react";
+import Link from "next/link";
 
-export default function Home() {
-  return <Button variant="destructive">Botãooo</Button>;
+export default async function Home() {
+  const { userId } = await auth();
+
+  const isAuth = !!userId;
+  return (
+    <div className="w-screen h-screen">
+      <main className="flex flex-col h-full items-center justify-center">
+        <h1 className="text-slate-50 text-3xl mb-5">Welcome to ChatPDF</h1>
+
+        {isAuth && <Button>Go to chats</Button>}
+
+        {isAuth ? (
+          <h1>file</h1>
+        ) : (
+          <Link href="/auth/sign-in">
+            <Button>
+              Login to get started <LogIn className="w-4 h-4 ml-2" />
+            </Button>
+          </Link>
+        )}
+      </main>
+    </div>
+  );
 }
