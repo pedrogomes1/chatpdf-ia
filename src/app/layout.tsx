800cec81656f4a3f7eddd01d7d4745f83e0abed6
Pedro Gomes <pedro-gomes@ibm.com>
2023-09-08 14:54:03 -0300

    chore: added conditional to display header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,34 @@
-import { ClerkProvider } from "@clerk/nextjs";
-import "./global.css";
+import { ClerkProvider, auth } from "@clerk/nextjs";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+import "./global.css";
+import Header from "@/components/header";
+
+const poppins = Poppins({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
   title: "ChatPDF",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { userId } = await auth();
+
+  const isAuth = !!userId;
+
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body
+          className={`${poppins.className} bg-gray-900 text-slate-50 px-5 py-5`}
+        >
+          {isAuth && <Header />}
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
