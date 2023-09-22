3ee96a6e0a31e2e3f1e6614fe20cf2fcf4fc3cbc
Pedro Gomes <pedro-gomes@ibm.com>
2023-09-22 14:41:11 -0300

    chore: update downloadFromS3 util function to sdk v3

--- src/lib/s3-server.ts
+++ src/lib/s3-server.ts
@@ -1,36 +1,40 @@
-import AWS from "aws-sdk";
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import fs from "fs";
 
+const client = new S3Client({
+  region: "us-east-1",
+  credentials: {
+    accessKeyId: process.env.NEXT_PUBLIC_AWS_S3_ACCESS_KEY!,
+    secretAccessKey: process.env.NEXT_PUBLIC_AWS_S3_SECRET_KEY!,
+  },
+});
+
 export async function downloadFromS3(fileKey: string) {
-  try {
-    AWS.config.update({
-      region: "us-east-1",
-      credentials: {
-        accessKeyId: process.env.NEXT_PUBLIC_AWS_S3_ACCESS_KEY!,
-        secretAccessKey: process.env.NEXT_PUBLIC_AWS_S3_SECRET_KEY!,
-      },
-    });
-
-    const s3 = new AWS.S3({
-      params: {
-        Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME,
-      },
-      region: process.env.NEXT_PUBLIC_AWS_S3_REGION,
-    });
-
-    const params = {
-      Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME!,
+  return new Promise(async (resolve, reject) => {
+    const command = new GetObjectCommand({
+      Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME,
       Key: fileKey,
-    };
+    });
 
-    const obj = await s3.getObject(params).promise();
+    try {
+      const { Body } = await client.send(command);
 
-    const file_name = `/tmp/pdf-${Date.now()}.pdf`;
-    fs.writeFileSync(file_name, obj.Body as Buffer);
+      const file_name = `/tmp/pdf-${Date.now().toString()}.pdf`;
 
-    return file_name;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+      if (Body instanceof require("stream").Readable) {
+        const file = fs.createWriteStream(file_name);
+        file.on("open", function (fd) {
+          // @ts-ignore
+          Body?.pipe(file).on("finish", () => {
+            return resolve(file_name);
+          });
+        });
+      }
+
+      return file_name;
+    } catch (error) {
+      console.error("ERROR DOWNLOAD FROM S3", error);
+      return null;
+    }
+  });
 }
