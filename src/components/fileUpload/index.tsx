272f249bf64a60dd41934ae35e1b2ea5c17ee701
Pedro Gomes <pedro-gomes@ibm.com>
2023-09-22 14:41:40 -0300

    chore: added mutate to FileUpload component

--- src/components/fileUpload/index.tsx
+++ src/components/fileUpload/index.tsx
@@ -1,12 +1,34 @@
 "use client";
+import React, { useState } from "react";
+import axios from "axios";
 import { uploadToS3 } from "@/lib/s3";
-import { Inbox } from "lucide-react";
-import React from "react";
+import { useMutation } from "@tanstack/react-query";
+import { Inbox, Loader2 } from "lucide-react";
 import { useDropzone } from "react-dropzone";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 const FILESIZE_ALLOWED = 10 * 1024 * 1024; //10MB
 
 export function FileUpload() {
+  const router = useRouter();
+  const [isUploading, setIsUploading] = useState(false);
+  const { mutate, isLoading } = useMutation({
+    mutationFn: async ({
+      file_key,
+      file_name,
+    }: {
+      file_key: string;
+      file_name: string;
+    }) => {
+      const response = await axios.post("/api/create-chat", {
+        file_key,
+        file_name,
+      });
+      return response.data;
+    },
+  });
+
   const { getRootProps, getInputProps } = useDropzone({
     maxFiles: 1,
     multiple: false,
@@ -15,14 +37,29 @@ export function FileUpload() {
       const file = acceptedFile[0];
 
       if (file.size > FILESIZE_ALLOWED) {
-        alert("Please, upload a smaller file");
+        return toast.error("Please, upload a smaller file");
       }
 
       try {
+        setIsUploading(true);
         const data = await uploadToS3(file);
-        console.log("data", data);
+        if (!data?.file_key || !data.file_name) {
+          return toast.error('Something went wrong"');
+        }
+
+        mutate(data, {
+          onSuccess: ({ chat_id }) => {
+            toast.success("Chat successful created");
+            router.push(`/chat/${chat_id}`);
+          },
+          onError: (err) => {
+            toast.error("Error creating chat");
+          },
+        });
       } catch (error) {
-        console.error(error);
+        toast.error("Error to upload pdf");
+      } finally {
+        setIsUploading(false);
       }
     },
   });
@@ -36,9 +73,14 @@ export function FileUpload() {
         })}
       >
         <input {...getInputProps()} />
-
-        <Inbox className="w-10 h-10 text-purple-500" />
-        <p className="mt-2 text-sm text-slate-400"> Drop PDF Here</p>
+        {isLoading || isUploading ? (
+          <Loader2 className="h-10 w-10 text-blue-500 animate-spin" />
+        ) : (
+          <>
+            <Inbox className="w-10 h-10 text-purple-500" />
+            <p className="mt-2 text-sm text-slate-400"> Drop PDF Here</p>
+          </>
+        )}
       </div>
     </div>
   );
