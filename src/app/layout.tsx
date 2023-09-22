37e521b54f7e2c25d3d98493137da39aa0d350b0
Pedro Gomes <pedro-gomes@ibm.com>
2023-09-22 09:35:37 -0300

    chore: update import order Providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,10 @@ import { ClerkProvider, auth } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import Header from "@/components/header";
+import { Providers } from "./store/providers";
+import { Toaster } from "react-hot-toast";
 
 import "./global.css";
-import { Providers } from "./store/providers";
 
 const poppins = Poppins({ subsets: ["latin"], weight: "400" });
 
@@ -22,17 +23,17 @@ export default async function RootLayout({
   const isAuth = !!userId;
 
   return (
-    <ClerkProvider>
-      <Providers>
-        <html lang="en">
-          <body
-            className={`${poppins.className} bg-gray-900 text-slate-50 px-5 py-5`}
-          >
-            {isAuth && <Header />}
-            {children}
-          </body>
-        </html>
-      </Providers>
-    </ClerkProvider>
+    <html lang="en">
+      <body
+        className={`${poppins.className} bg-gray-900 text-slate-50 px-5 py-5`}
+      >
+        <ClerkProvider>
+          {isAuth && <Header />}
+
+          <Providers>{children}</Providers>
+        </ClerkProvider>
+        <Toaster />
+      </body>
+    </html>
   );
 }
