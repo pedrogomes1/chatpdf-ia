a92141b1f8c210fb6b6bd3040324c5fe65998ad4
Pedro Gomes <pedro-gomes@ibm.com>
2023-09-22 14:40:14 -0300

    feat: create new util function getEmbeddings

---
+++ src/lib/embeddings.ts
@@ -0,0 +1,22 @@
+import { OpenAIApi, Configuration } from "openai-edge";
+
+const config = new Configuration({
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_KEY!,
+});
+
+const openai = new OpenAIApi(config);
+
+//convert text in a vector
+export async function getEmbeddings(text: String) {
+  try {
+    const response = await openai.createEmbedding({
+      model: "text-embedding-ada-002",
+      input: text.replace(/\n/g, " "),
+    });
+
+    const result = await response.json();
+    return result.data[0].embedding as number[];
+  } catch (error) {
+    console.error("error calling openai embeddings api", error);
+  }
+}
