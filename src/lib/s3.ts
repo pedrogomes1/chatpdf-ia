1680f7d9699a22bc6e428985085ce4cd87b47ccb
Pedro Gomes <pedro-gomes@ibm.com>
2023-09-22 14:40:31 -0300

    chore: update s3 upload do sdk v3

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,45 +1,29 @@
-import AWS from "aws-sdk";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+
+const client = new S3Client({
+  region: "us-east-1",
+  credentials: {
+    accessKeyId: process.env.NEXT_PUBLIC_AWS_S3_ACCESS_KEY!,
+    secretAccessKey: process.env.NEXT_PUBLIC_AWS_S3_SECRET_KEY!,
+  },
+});
 
 export async function uploadToS3(file: File) {
+  const FILE_KEY =
+    "uploads/" + Date.now().toString() + file.name.replace(" ", "-");
+
+  const command = new PutObjectCommand({
+    Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME,
+    Key: FILE_KEY,
+    Body: file,
+  });
+
   try {
-    AWS.config.update({
-      region: "us-east-1",
-      credentials: {
-        accessKeyId: process.env.NEXT_PUBLIC_AWS_S3_ACCESS_KEY!,
-        secretAccessKey: process.env.NEXT_PUBLIC_AWS_S3_SECRET_KEY!,
-      },
-    });
-
-    const FILE_KEY =
-      "uploads/" + Date.now().toString() + file.name.replace(" ", "-");
-    const s3 = new AWS.S3({
-      params: {
-        Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME,
-      },
-      region: process.env.NEXT_PUBLIC_AWS_S3_REGION,
-    });
-
-    const params = {
-      Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME!,
-      Key: FILE_KEY,
-      Body: file,
-    };
-
-    const upload = s3
-      .putObject(params)
-      .on("httpUploadProgress", (event) => {
-        console.log(event);
-      })
-      .promise();
-
-    await upload.then((data) => {
-      console.log("aheo");
-    });
-
-    return Promise.resolve({
+    await client.send(command);
+    return {
       file_key: FILE_KEY,
       file_name: file.name,
-    });
+    };
   } catch (error) {
     console.error("error upload", error);
   }
