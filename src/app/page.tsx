42f3fac442b81bf358c927e203bac681544ecae7
Pedro Gomes <pedro-gomes@ibm.com>
2023-10-03 13:47:45 -0300

    chore: added button redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,15 @@ import { auth } from "@clerk/nextjs";
 import Link from "next/link";
 import { FileUpload } from "@/components/fileUpload";
 import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+import { chats } from "@/lib/db/schema";
 
 export default async function Home() {
   const { userId } = await auth();
+
+  const _chats = await db.select().from(chats);
+
+  const lastChat = _chats.length ? _chats[_chats.length - 1] : null;
 
   const isAuth = !!userId;
   return (
@@ -13,7 +19,13 @@ export default async function Home() {
       <main className="flex flex-col gap-5 w-full max-w-lg mx-auto h-full items-center justify-center">
         <h1 className="text-slate-50 text-3xl mb-5">Welcome to ChatPDF</h1>
 
-        {isAuth && <Button variant="secondary">Go to chats</Button>}
+        {isAuth && (
+          <Link href={`/chat/${lastChat?.id}`}>
+            <Button disabled={!lastChat} variant="secondary">
+              Go to chats
+            </Button>
+          </Link>
+        )}
 
         {isAuth ? (
           <FileUpload />
